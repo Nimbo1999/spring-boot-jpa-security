ff9e9529f2a28f55b64876330277fbb666eb8115
Matheus Lopes <matlopes1999@gmail.com>
2021-11-04 20:58:46 -0300

    Initialize project

---
+++ src/main/java/io/github/nimbo1999/Nimbo1999Application.java
@@ -0,0 +1,13 @@
+package io.github.nimbo1999;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nimbo1999Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nimbo1999Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nimbo1999/Nimbo1999ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nimbo1999;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nimbo1999ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
